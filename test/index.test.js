0dad599f3870bc6fe261e0da04db213574648ca8
Marcio Silva <marcio.silva@ibm.com>
2023-07-31 09:30:46 -0300

    perform the 'canary' test just to check if the structure works

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,11 @@
 const { high } = require('../src/index');
 
+describe('the high canary spec', () => {
+  it('shows the infrastructure works', () => {
+    expect(true).toBe(true);
+  });
+});
+
 test('high', () => {
   expect(high(0)).toBe(true);
 });
